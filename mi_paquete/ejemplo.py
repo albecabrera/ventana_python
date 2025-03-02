07193c21119c553d46e6ae30d9367c448c025025
albecabrera <albe.cabrera@yahoo.de>
2025-03-02 21:47:37 +0100

    tercer cambio

---
+++ mi_paquete/ejemplo.py
@@ -0,0 +1,5 @@
+import mi_modulo
+
+suma = mi_modulo.suma(3, 4)
+
+print(suma)
--- mi_paquete/mi_modulo.py
+++ mi_paquete/mi_modulo.py
@@ -2,7 +2,7 @@ def suma(a, b):
     return a + b
 
 2 usages
-class Perro()
+class Perro():
     def __init__(self, nombre):
         self.nombre = nombre
     
